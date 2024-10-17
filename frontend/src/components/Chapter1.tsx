e54e9ba9f70b2e92feb68d683d770a229a2ec600
nxshil <sahil.natikar@gmail.com>
2024-10-18 00:47:25 +0530

    lol

--- frontend/src/components/Chapter1.tsx
+++ frontend/src/components/Chapter1.tsx
@@ -140,7 +140,7 @@ const Chapter1: React.FC = () => {
     return () => clearInterval(intervalId);
   }, [user?.startTime]);
 
-  console.log(user);
+  // console.log(user);
   const startGame = () => {
     const currentDateTime = new Date();
     const accessStartTime = new Date("2024-10-18T00:00:00");
